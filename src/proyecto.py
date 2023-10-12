5c2012114cd1d4b5a4cae0e06bbbd5e821f99996
PieroAGP <xpgabpar357@ieshnosmachado.org>
2023-10-12 22:24:49 +0200

    almacenar nombres utilizando una función

--- src/proyecto.py
+++ src/proyecto.py
@@ -1,17 +1,29 @@
 import requests
 import json
 
-#le paso una url
-url = "https://api.gameofthronesquotes.xyz/v1/random/5"
-#llamada a la api
-respuesta=requests.get(url)
-#si la api me devuelve algo entro en el if
-if respuesta.status_code == 200:
-    #convierto en un jason
-    datoPersonajes=respuesta.json()
-    #recorro los datos
-    for dato in datoPersonajes:
-        #almaceno solo el nombre 
-        nombrePersonaje=dato["character"]["slug"]
-        #muesto por consola el resultado
-        print(nombrePersonaje)
+#creo una funcion para llamar a la api para que nos devuelva los nombres de los personajes
+def obtenerPersonajes():
+    #le paso una url
+    url = "https://api.gameofthronesquotes.xyz/v1/random/5"
+    #llamada a la api
+    respuesta=requests.get(url)
+    #si la api me devuelve algo entro en el if
+    if respuesta.status_code == 200:
+        #creamos una lista para devolver solo nombres
+        listaNombre=[]
+        #convierto respuesta a un JSON
+        datoPersonajes=respuesta.json()
+        #recorro los datos
+        for dato in datoPersonajes:
+            #almaceno solo el nombre 
+            nombrePersonaje=dato["character"]["slug"]
+            #con el metodo .append añado a la lista
+            listaNombre.append(nombrePersonaje)
+        #devuelvo la lista con los nombres
+        return listaNombre
+
+#declaramos una variable de tipo lista para almacenar lo que devuelve el funcion
+nuevaLista=obtenerPersonajes()
+print("Nombres:")
+for nombre in nuevaLista:
+    print(nombre)
