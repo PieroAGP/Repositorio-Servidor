b25c9526171d9e6ff34c609bcce88bce17102710
PieroAGP <xpgabpar357@ieshnosmachado.org>
2023-10-13 19:47:56 +0200

    Almacenamiento completo/selección aleatoria

--- src/proyecto.py
+++ src/proyecto.py
@@ -1,10 +1,11 @@
 import requests
 import json
+import random
 
 #creo una funcion para llamar a la api para que nos devuelva los nombres de los personajes
 def obtenerPersonajes():
     #le paso una url
-    url = "https://api.gameofthronesquotes.xyz/v1/random/5"
+    url = "https://api.gameofthronesquotes.xyz/v1/random/3"
     #llamada a la api
     respuesta=requests.get(url)
     #si la api me devuelve algo entro en el if
@@ -22,8 +23,35 @@ def obtenerPersonajes():
         #devuelvo la lista con los nombres
         return listaNombre
 
-#declaramos una variable de tipo lista para almacenar lo que devuelve el funcion
+#funcion para traer frases de los personajes pasado por parametro
+def funcionDatos(nombrePersonaje):
+    #declaramos una url nueva con el nombre personaje
+    urlNueva = f"https://api.gameofthronesquotes.xyz/v1/author/{nombrePersonaje}/3"
+    #llamada a la Api
+    respuesta=requests.get(urlNueva)
+    #si la api me devuelve algo entro en el if
+    if respuesta.status_code == 200:
+        #convierto respuesta a un JSON
+        datoPersonajes=respuesta.json()
+        #devuelvo una lista de objetos(con las frases de los personajes)
+        return datoPersonajes
+
+#declaramos una variable de tipo lista para almacenar lo que devuelve el funcion(nombres de personajes)
 nuevaLista=obtenerPersonajes()
-print("Nombres:")
-for nombre in nuevaLista:
-    print(nombre)
+#almacenar los nombres en variables
+nombre1,nombre2,nombre3=nuevaLista
+#almacenar listas de objestos de cada personaje
+lista1=funcionDatos(nombre1)
+lista2=funcionDatos(nombre2)
+lista3=funcionDatos(nombre3)
+#unir las lista para tener un base de datos
+listaCompleta=lista1+lista2+lista3
+
+#de toda la lista de objetos me da un aleatorio
+objetoAleatorio=random.choice(listaCompleta)
+#filtramos por frase y nombre del personaje
+frase=objetoAleatorio["sentence"]
+personaje=objetoAleatorio["character"]["name"]
+
+print(frase)
+print(personaje)
